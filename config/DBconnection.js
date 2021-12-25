61babe1723d998a6ea6e6f7460d57d5e58976050
AFZAL555 <afzalofficial@gmail.com>
2021-12-25 17:28:23 +0530

    version1

---
+++ config/DBconnection.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient
+const state={db:null}
+
+module.exports.connect = function (done) {
+    const url = "mongodb://127.0.0.1:27017";
+    const dbname = "Brocart"
+    MongoClient.connect(url, function (err, data) {
+        if (err) return done(err);
+        state.db=data.db(dbname);
+        done();
+    });
+
+    
+}
+
+module.exports.get=function(){
+    return state.db;
+}
---
+++ config/collections.js
@@ -0,0 +1,5 @@
+module.exports={
+    USER_COLLECTION:'users',
+    ADMIN_COLLECTION:'Admin'
+    
+}
